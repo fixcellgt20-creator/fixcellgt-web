74e98bc3b4228a43f31022a94f1b52b30a614af6
fixcellgt20-creator <fixcellgt20@gmail.com>
2026-08-18 08:00:43 -0600

    Solo HTML - eliminar todo Python de Vercel

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies with pip (not uv)..."
-python3 -m pip install --upgrade pip
-python3 -m pip install --no-cache-dir -r requirements.txt
-
-echo "Build complete!"
